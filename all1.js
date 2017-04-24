ed6f4887aafde69f6588de9123dc376e4a1e03a3
sravanipolani <sravani594@gmail.com>
2017-04-24 17:36:19 -0400

    Add files via upload

---
+++ all1.js
@@ -0,0 +1,10 @@
+var sum=require('./arthmatic');
+var sub=require('./arthmatic');
+var mul=require('./arthmatic');
+var div=require('./arthmatic');
+
+console.log(sum.sum(2,4));
+console.log(sub.sub(6,4));
+console.log(mul.mul(2,4));
+console.log(div.div(2,4));
+
---
+++ arthmatic.js
@@ -0,0 +1,13 @@
+exports.sum=function(x,y){
+return x+y;
+}
+
+exports.sub=function(x,y){
+return x-y;
+}
+exports.mul=function(x,y){
+return x*y;
+}
+exports.div=function(x,y){
+return x/y;
+}
